84325240c3cdb875ffea6a7301f2616b506efd1c
Amro Qadadha <amroqadadha@gmail.com>
2024-12-18 15:36:57 +0200

    Register connection string and identity Core

--- TAABP.API/Program.cs
+++ TAABP.API/Program.cs
@@ -1,6 +1,18 @@
+using TAABP.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using TAABP.Core;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'CarRentalSystemDbContextConnection' not found.");
+
+builder.Services.AddDbContext<TAABPDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentityCore<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<TAABPDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
