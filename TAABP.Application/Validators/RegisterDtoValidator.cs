6c1fa1347fe7a3cfda585da8f347072d8bfca07f
Amro Qadadha <amroqadadha@gmail.com>
2024-12-18 17:39:39 +0200

    Create RegisterDto and it's validator

---
+++ TAABP.Application/Validators/RegisterDtoValidator.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using TAABP.Application.DTOs;
+
+namespace TAABP.Application.Validators
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .WithMessage("Username is required")
+                .MinimumLength(5)
+                .WithMessage("Username must be at least 5 characters long");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long")
+                .Matches("[A-Z]")
+                .WithMessage("Password must contain at least one uppercase letter")
+                .Matches("[a-z]")
+                .WithMessage("Password must contain at least one lowercase letter")
+                .Matches("[0-9]")
+                .WithMessage("Password must contain at least one number")
+                .Matches("[^a-zA-Z0-9]")
+                .WithMessage("Password must contain at least one special character");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.Password)
+                .WithMessage("Passwords do not match");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("Phone number is required")
+                .Matches(@"^\+?[1-9]\d{1,14}$")
+                .WithMessage("Phone number must be in a valid format");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Invalid email address");
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required");
+        }
+    }
+}
