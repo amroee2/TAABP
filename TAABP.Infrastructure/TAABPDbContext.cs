521409b78572ef05b9b3ea4f493338baa8e5f146
Amro Qadadha <amroqadadha@gmail.com>
2024-12-18 15:29:42 +0200

    Create DbContext class

---
+++ TAABP.Infrastructure/TAABPDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TAABP.Core;
+
+namespace TAABP.Infrastructure
+{
+    public class TAABPDbContext : IdentityDbContext<User>
+    {
+        public TAABPDbContext(DbContextOptions<TAABPDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
