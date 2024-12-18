afff66632b8450c0a184a692d515b4ed952ffee9
Amro Qadadha <amroqadadha@gmail.com>
2024-12-18 17:53:15 +0200

    Implement Password Hashing

---
+++ TAABP.Application/PasswordHashing/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace TAABP.Application.PasswordHashing
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+    }
+}
---
+++ TAABP.Infrastructure/PasswordHasherService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TAABP.Application.PasswordHashing;
+
+namespace TAABP.Infrastructure
+{
+    public class PasswordHasherService : IPasswordHasher
+    {
+        private readonly PasswordHasher<IdentityUser> _passwordHasher;
+
+        public PasswordHasherService()
+        {
+            _passwordHasher = new PasswordHasher<IdentityUser>();
+        }
+
+        public string HashPassword(string password)
+        {
+            return _passwordHasher.HashPassword(new IdentityUser(), password);
+        }
+    }
+}
