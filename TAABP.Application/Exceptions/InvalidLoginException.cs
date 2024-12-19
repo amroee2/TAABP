5ef74854441557cc97cbfdca22f8ad54316ed01f
Amro Qadadha <amroqadadha@gmail.com>
2024-12-19 19:13:27 +0200

    Create LoginDto and Invalid Login Exception

---
+++ TAABP.Application/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,17 @@
+﻿namespace TAABP.Application.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public InvalidLoginException(string message) : base(message)
+        {
+        }
+
+        public InvalidLoginException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public InvalidLoginException()
+        {
+        }
+    }
+}
