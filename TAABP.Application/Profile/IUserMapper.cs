903ebd8cb21094dcf126d24cdac182bb91bddf51
Amro Qadadha <amroqadadha@gmail.com>
2024-12-20 19:11:45 +0200

    Update user mapper

--- TAABP.Application/Profile/IUserMapper.cs
+++ TAABP.Application/Profile/IUserMapper.cs
@@ -7,6 +7,6 @@ namespace TAABP.Application.Profile
     {
         User RegisterDtoToUser(RegisterDto registerDto);
         UserDto UserToUserDto(User user);
-        User UserDtoToUser(UserDto userDto);
+        void UserDtoToUser(UserDto userDto, User user);
     }
 }
--- TAABP.Application/Profile/UserMapper.cs
+++ TAABP.Application/Profile/UserMapper.cs
@@ -5,10 +5,11 @@ using TAABP.Core;
 namespace TAABP.Application.Profile
 {
     [Mapper]
+
     public partial class UserMapper : IUserMapper
     {
         public partial User RegisterDtoToUser(RegisterDto registerDto);
         public partial UserDto UserToUserDto(User user);
-        public partial User UserDtoToUser(UserDto userDto);
+        public partial void UserDtoToUser(UserDto userDto, User user);
     }
 }
