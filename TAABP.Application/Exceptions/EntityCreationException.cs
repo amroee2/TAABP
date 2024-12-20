867e97685a1e026df58e1c52e59ccb63a402f33c
Amro Qadadha <amroqadadha@gmail.com>
2024-12-20 17:06:47 +0200

    Add entity creation exception

---
+++ TAABP.Application/Exceptions/EntityCreationException.cs
@@ -0,0 +1,17 @@
+﻿namespace TAABP.Application.Exceptions
+{
+    public class EntityCreationException : Exception
+    {
+        public EntityCreationException(string message) : base(message)
+        {
+        }
+
+        public EntityCreationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public EntityCreationException()
+        {
+        }
+    }
+}
