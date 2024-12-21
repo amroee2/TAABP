083280c0cc0cb4d64b9ff1e36e88e1465d7f1787
Amro Qadadha <amroqadadha@gmail.com>
2024-12-21 18:57:28 +0200

    Add new Hotel Image selection api endpoint

--- TAABP.API/Controllers/HotelImageController.cs
+++ TAABP.API/Controllers/HotelImageController.cs
@@ -29,5 +29,26 @@ namespace TAABP.API.Controllers
             }
             return Ok();
         }
+
+        [HttpGet("{imageId}")]
+        public async Task<IActionResult> GetHotelImage(int id, int imageId)
+        {
+            try
+            {
+                var hotelImage = await _hotelService.GetHotelImage(id, imageId);
+                return Ok(hotelImage);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHotelImages(int id)
+        {
+            var hotelImages = await _hotelService.GetHotelImages(id);
+            return Ok(hotelImages);
+        }
     }
 }
