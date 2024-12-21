bb2afa9a78b63fa87180cb73ee2034315558a6da
Amro Qadadha <amroqadadha@gmail.com>
2024-12-21 16:36:52 +0200

    Implement hotel creation api endpoint

---
+++ TAABP.API/Controllers/HotelController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TAABP.Application.DTOs;
+using TAABP.Application.ServiceInterfaces;
+
+namespace TAABP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IHotelService _hotelService;
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHotelAsync(HotelDto hotelDto)
+        {
+            await _hotelService.CreateHotelAsync(hotelDto);
+            return Ok();
+        }
+    }
+}
