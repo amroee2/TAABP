da500a2cfdb8f484b151da4b2b40010d391151b4
Amro Qadadha <amroqadadha@gmail.com>
2024-12-21 18:47:53 +0200

    Add insert new hotel image api endpoint

---
+++ TAABP.API/Controllers/HotelImageController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TAABP.Application.DTOs;
+using TAABP.Application.Exceptions;
+using TAABP.Application.ServiceInterfaces;
+
+namespace TAABP.API.Controllers
+{
+    [Route("api/Hotels/{id}/[controller]")]
+    [ApiController]
+    public class HotelImageController : ControllerBase
+    {
+        private readonly IHotelService _hotelService;
+        public HotelImageController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewImageAsync(int id, HotelImageDto hotelImageDto)
+        {
+            try
+            {
+                await _hotelService.AddNewImageAsync(id, hotelImageDto);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            return Ok();
+        }
+    }
+}
