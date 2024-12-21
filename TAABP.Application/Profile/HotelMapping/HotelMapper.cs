3e66a93ff53f126241d5e260d4c88109dc0c39b3
Amro Qadadha <amroqadadha@gmail.com>
2024-12-21 15:30:43 +0200

    Add new Hotel mapper

---
+++ TAABP.Application/Profile/HotelMapping/HotelMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.HotelMapping
+{
+    [Mapper]
+    public partial class HotelMapper : IHotelMapper
+    {
+        public partial Hotel HotelDtoToUser(HotelDto hotelDto);
+
+    }
+}
---
+++ TAABP.Application/Profile/HotelMapping/IHotelMapper.cs
@@ -0,0 +1,10 @@
+﻿using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.HotelMapping
+{
+    public interface IHotelMapper
+    {
+        Hotel HotelDtoToUser(HotelDto hotelDto);
+    }
+}
