9ae77b2d4022ef86b853de9a7ff984e6640f9b96
Amro Qadadha <amroqadadha@gmail.com>
2024-12-21 14:49:53 +0200

    Create new Hotel Entity

---
+++ TAABP.Core/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace TAABP.Core
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        public string Owner { get; set; }
+        public int Rating { get; set; }
+        public int NumberOfRooms { get; set; }
+    }
+}
