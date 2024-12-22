6d0006500b2c6b8d899c9ff83f7901b8bec9bb0b
Amro Qadadha <amroqadadha@gmail.com>
2024-12-22 16:20:02 +0200

    Add amenity validator

---
+++ TAABP.Application/Validators/AmenityDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TAABP.Application.DTOs;
+
+namespace TAABP.Application.Validators
+{
+    public class AmenityDtoValidator : AbstractValidator<AmenityDto>
+    {
+        public AmenityDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can't be empty");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description can't be empty");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Image is required");
+            RuleFor(x => x.HotelId).NotEmpty().WithMessage("Hotel id is required");
+        }
+    }
+}
