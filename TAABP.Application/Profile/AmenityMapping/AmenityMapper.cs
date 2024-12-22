9d93fd5f0e7b02c19626e49763cf4f58f96cdfbc
Amro Qadadha <amroqadadha@gmail.com>
2024-12-22 16:17:33 +0200

    Add Amenity Mapping

---
+++ TAABP.Application/Profile/AmenityMapping/AmenityMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.AmenityMapping
+{
+    [Mapper]
+    public partial class AmenityMapper : IAmenityMapper
+    {
+        public partial Amenity AmenityDtoToAmenity(AmenityDto amenityDto);
+        public partial AmenityDto AmenityToAmenityDto(Amenity amenity);
+    }
+}
---
+++ TAABP.Application/Profile/AmenityMapping/IAmenityMapper.cs
@@ -0,0 +1,11 @@
+﻿using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.AmenityMapping
+{
+    public partial interface IAmenityMapper
+    {
+        Amenity AmenityDtoToAmenity(AmenityDto amenityDto);
+        AmenityDto AmenityToAmenityDto(Amenity amenity);
+    }
+}
