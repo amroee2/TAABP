130a7baa30bfec1baae1b1677e962520f25da88a
Amro Qadadha <amroqadadha@gmail.com>
2024-12-22 16:12:32 +0200

    Add and Migrate new Amenity Entity

---
+++ TAABP.Core/Amenity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TAABP.Core
+{
+    public class Amenity
+    {
+        [Key]
+        public int AmenityId { get; set; }
+        [Required(ErrorMessage = "Please enter the name of the amenity")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a description of the amenity")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter the image URL")]
+        public string ImageUrl { get; set; }
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
