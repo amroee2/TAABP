3f83060e7a9c3a020b64b7698d365f288ed9133e
Amro Qadadha <amroqadadha@gmail.com>
2024-12-22 16:13:35 +0200

    Add Amenity repository crud operations

---
+++ TAABP.Application/RepositoryInterfaces/IAmenityRepository.cs
@@ -0,0 +1,13 @@
+﻿using TAABP.Core;
+
+namespace TAABP.Application.RepositoryInterfaces
+{
+    public interface IAmenityRepository
+    {
+        Task<List<Amenity>> GetHotelAmenitiesAsync(int hotelId);
+        Task<Amenity> GetAmenityAsync(int amenityId);
+        Task<Amenity> CreateAmenityAsync(Amenity amenity);
+        Task<Amenity> UpdateAmenityAsync(Amenity amenity);
+        Task DeleteAmenityAsync(Amenity amenity);
+    }
+}
---
+++ TAABP.Infrastructure/Repositories/AmenityRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TAABP.Application.RepositoryInterfaces;
+using TAABP.Core;
+
+namespace TAABP.Infrastructure.Repositories
+{
+    public class AmenityRepository : IAmenityRepository
+    {
+        private readonly TAABPDbContext _context;
+
+        public AmenityRepository(TAABPDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Amenity>> GetHotelAmenitiesAsync(int hotelId)
+        {
+            return await _context.Amenities
+                .Where(a => a.HotelId == hotelId)
+                .ToListAsync();
+        }
+
+        public async Task<Amenity> GetAmenityAsync(int amenityId)
+        {
+            return await _context.Amenities
+                .FirstOrDefaultAsync(a => a.AmenityId == amenityId);
+        }
+
+        public async Task<Amenity> CreateAmenityAsync(Amenity amenity)
+        {
+            _context.Amenities.Add(amenity);
+            await _context.SaveChangesAsync();
+            return amenity;
+        }
+
+        public async Task<Amenity> UpdateAmenityAsync(Amenity amenity)
+        {
+            _context.Amenities.Update(amenity);
+            await _context.SaveChangesAsync();
+            return amenity;
+        }
+
+        public async Task DeleteAmenityAsync(Amenity amenity)
+        {
+            _context.Amenities.Remove(amenity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
