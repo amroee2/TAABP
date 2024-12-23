01c521f7a6658b869c64a85e3d09b0961c5d7ac2
Amro Qadadha <amroqadadha@gmail.com>
2024-12-23 12:18:32 +0200

    Add new Room Entity Mapping

---
+++ TAABP.Application/Profile/RoomMapping/IRoomMapper.cs
@@ -0,0 +1,11 @@
+﻿using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.RoomMapping
+{
+    public partial interface IRoomMapper
+    {
+        Room RoomDtoToRoom(RoomDto roomDto);
+        RoomDto RoomToRoomDto(Room room);
+    }
+}
---
+++ TAABP.Application/Profile/RoomMapping/RoomMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.RoomMapping
+{
+    [Mapper]
+    public partial class RoomMapper : IRoomMapper
+    {
+        public partial Room RoomDtoToRoom(RoomDto roomDto);
+        public partial RoomDto RoomToRoomDto(Room room);
+    }
+}
