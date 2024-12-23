cc1bb7e1267e8fa01e1dd5dd644fb2a4cf76819e
Amro Qadadha <amroqadadha@gmail.com>
2024-12-23 11:53:36 +0200

    Add room curd operations repository method

---
+++ TAABP.Application/RepositoryInterfaces/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using TAABP.Core;
+
+namespace TAABP.Application.RepositoryInterfaces
+{
+    public interface IRoomRepository
+    {
+        Task<Room> GetRoomByIdAsync(int id);
+        Task<List<Room>> GetRoomsAsync();
+        Task CreateRoomAsync(Room room);
+        Task UpdateRoomAsync(Room room);
+        Task DeleteRoomAsync(Room room);
+    }
+}
---
+++ TAABP.Infrastructure/Repositories/RoomRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TAABP.Application.RepositoryInterfaces;
+using TAABP.Core;
+
+namespace TAABP.Infrastructure.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly TAABPDbContext _context;
+
+        public RoomRepository(TAABPDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Room> GetRoomByIdAsync(int id)
+        {
+            return await _context.Rooms.AsNoTracking().FirstOrDefaultAsync(r => r.RoomId == id);
+        }
+
+        public async Task<List<Room>> GetRoomsAsync()
+        {
+            return await _context.Rooms.AsNoTracking().ToListAsync();
+        }
+
+        public async Task CreateRoomAsync(Room room)
+        {
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateRoomAsync(Room room)
+        {
+            _context.Update(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoomAsync(Room room)
+        {
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
