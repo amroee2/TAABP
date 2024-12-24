275ed555a1f195576298b4fff5706060ce3c4357
Amro Qadadha <amroqadadha@gmail.com>
2024-12-24 15:43:29 +0200

    Add new CityDto with mapping

---
+++ TAABP.Application/Profile/CityMapping/ICityMapper.cs
@@ -0,0 +1,11 @@
+﻿using TAABP.Application.DTOs;
+using TAABP.Core;
+
+namespace TAABP.Application.Profile.CityMapping
+{
+    public interface ICityMapper
+    {
+        public City CityDtoToCity(CityDto cityDto);
+        CityDto CityToCityDto(City city);
+    }
+}
