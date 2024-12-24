261c6c4f2acb3f1bc5d89f0291badb274705dd33
Amro Qadadha <amroqadadha@gmail.com>
2024-12-24 13:42:16 +0200

    Add FeaturedDealDto with Mapping

---
+++ TAABP.Application/DTOs/FeatueredDealDto.cs
@@ -0,0 +1,17 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace TAABP.Application.DTOs
+{
+    [Mapper]
+    public class FeatueredDealDto
+    {
+        public int FeaturedDealId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Discount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public int RoomId { get; set; }
+    }
+}
