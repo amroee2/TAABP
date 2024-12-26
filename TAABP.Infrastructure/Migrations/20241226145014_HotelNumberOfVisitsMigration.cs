4c59ec389510da110719f0b5ae8ae44693feeea7
Amro Qadadha <amroqadadha@gmail.com>
2024-12-26 16:50:54 +0200

    Add new NumberOfVisits column to the database

---
+++ TAABP.Infrastructure/Migrations/20241226145014_HotelNumberOfVisitsMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TAABP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelNumberOfVisitsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfVisits",
+                table: "Hotels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Hotel_NumberOfVisits_Positive",
+                table: "Hotels",
+                sql: "[NumberOfVisits] >= 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Hotel_NumberOfVisits_Positive",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfVisits",
+                table: "Hotels");
+        }
+    }
+}
