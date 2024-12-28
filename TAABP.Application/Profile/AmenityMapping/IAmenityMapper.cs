b0e7225c4fc0323444d7d65b5e7768e8134b7942
Amro Qadadha <amroqadadha@gmail.com>
2024-12-28 19:12:16 +0200

    Refactor amenity mapping approach

--- TAABP.Application/Profile/AmenityMapping/IAmenityMapper.cs
+++ TAABP.Application/Profile/AmenityMapping/IAmenityMapper.cs
@@ -5,7 +5,7 @@ namespace TAABP.Application.Profile.AmenityMapping
 {
     public partial interface IAmenityMapper
     {
-        Amenity AmenityDtoToAmenity(AmenityDto amenityDto);
+        void AmenityDtoToAmenity(AmenityDto amenityDto, Amenity amenity);
         AmenityDto AmenityToAmenityDto(Amenity amenity);
     }
 }
