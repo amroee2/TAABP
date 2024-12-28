3b8e905a885c5648893caedf3f57f5690fe703d3
Amro Qadadha <amroqadadha@gmail.com>
2024-12-28 19:14:40 +0200

    Refactor amenity crud operations service methods

--- TAABP.Application/Services/AmenityService.cs
+++ TAABP.Application/Services/AmenityService.cs
@@ -30,54 +30,51 @@ namespace TAABP.Application.Services
             return amenities.Select(amenity => _amenityMapper.AmenityToAmenityDto(amenity)).ToList();
         }
 
-        public async Task<AmenityDto> GetAmenityAsync(int amenityId)
+        public async Task<AmenityDto> GetAmenityByIdAsync(int hotelId, int amenityId)
         {
-            var amenity = await _amenityRepository.GetAmenityAsync(amenityId);
-            if (amenity == null)
+            var amenity = await _amenityRepository.GetAmenityByIdAsync(amenityId);
+            var hotel = await _hotelRepository.GetHotelByIdAsync(hotelId);
+            if (amenity == null || hotel == null || hotel.HotelId!= amenity.HotelId)
             {
                 throw new EntityNotFoundException("Amenity not found");
             }
             return _amenityMapper.AmenityToAmenityDto(amenity);
         }
 
-        public async Task CreateAmenityAsync(AmenityDto amenity)
+        public async Task<int> CreateAmenityAsync(AmenityDto amenityDto)
         {
-            var hotel = await _hotelRepository.GetHotelByIdAsync(amenity.HotelId);
+            var hotel = await _hotelRepository.GetHotelByIdAsync(amenityDto.HotelId);
             if (hotel == null)
             {
                 throw new EntityNotFoundException("Hotel not found");
             }
-            await _amenityRepository.CreateAmenityAsync(_amenityMapper.AmenityDtoToAmenity(amenity));
+            var amenity = new Amenity();
+            _amenityMapper.AmenityDtoToAmenity(amenityDto, amenity);
+            await _amenityRepository.CreateAmenityAsync(amenity);
+            return amenity.AmenityId;
         }
 
         public async Task UpdateAmenityAsync(AmenityDto amenity)
         {
             var hotel = await _hotelRepository.GetHotelByIdAsync(amenity.HotelId);
-            var targetAmenity = await _amenityRepository.GetAmenityAsync(amenity.AmenityId);
-            if (hotel == null)
-            {
-                throw new EntityNotFoundException("Hotel not found");
-            }
-            if (targetAmenity == null)
+            var targetAmenity = await _amenityRepository.GetAmenityByIdAsync(amenity.AmenityId);
+            if (targetAmenity == null || hotel == null || hotel.HotelId != targetAmenity.HotelId)
             {
                 throw new EntityNotFoundException("Amenity not found");
             }
-            await _amenityRepository.UpdateAmenityAsync(_amenityMapper.AmenityDtoToAmenity(amenity));
+            _amenityMapper.AmenityDtoToAmenity(amenity, targetAmenity);
+            await _amenityRepository.UpdateAmenityAsync(targetAmenity);
         }
 
-        public async Task DeleteAmenityAsync(AmenityDto amenity)
+        public async Task DeleteAmenityAsync(int hotelId, int amenityId)
         {
-            var hotel = await _hotelRepository.GetHotelByIdAsync(amenity.HotelId);
-            var targetAmenity = await _amenityRepository.GetAmenityAsync(amenity.AmenityId);
-            if (hotel == null)
-            {
-                throw new EntityNotFoundException("Hotel not found");
-            }
-            if (targetAmenity == null)
+            var hotel = await _hotelRepository.GetHotelByIdAsync(hotelId);
+            var targetAmenity = await _amenityRepository.GetAmenityByIdAsync(amenityId);
+            if (targetAmenity == null || hotel == null || hotel.HotelId != targetAmenity.HotelId)
             {
                 throw new EntityNotFoundException("Amenity not found");
             }
-            await _amenityRepository.DeleteAmenityAsync(_amenityMapper.AmenityDtoToAmenity(amenity));
+            await _amenityRepository.DeleteAmenityAsync(targetAmenity);
         }
     }
 }
