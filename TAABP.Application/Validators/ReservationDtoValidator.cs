ad22153973d4092e4c22f3a0831de132abb98b1c
Amro Qadadha <amroqadadha@gmail.com>
2024-12-29 13:47:59 +0200

    Rename ReservationValidator

---
+++ TAABP.Application/Validators/ReservationDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TAABP.Application.DTOs;
+
+namespace TAABP.Application.Validators
+{
+    public class ReservationDtoValidator : AbstractValidator<ReservationDto>
+    {
+        public ReservationDtoValidator()
+        {
+            RuleFor(r => r.StartDate).NotEmpty().WithMessage("Start date is required")
+                .GreaterThan(System.DateTime.Now)
+                .WithMessage("Start date must be greater than today");
+            RuleFor(x => x.EndDate)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .GreaterThan(x => x.StartDate).WithMessage("{PropertyName} must be greater than Start Date");
+        }
+    }
+}
