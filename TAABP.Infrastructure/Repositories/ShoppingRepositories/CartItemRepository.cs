247ab833f0330443977bd54e5fb49414a03bba59
Amro Qadadha <amroqadadha@gmail.com>
2025-01-10 19:21:06 +0200

    Add new CartItem and Cart repositories

---
+++ TAABP.Infrastructure/Repositories/ShoppingRepositories/CartItemRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TAABP.Core.ShoppingEntities;
+
+namespace TAABP.Infrastructure.Repositories.ShoppingRepositories
+{
+    public class CartItemRepository
+    {
+        private readonly TAABPDbContext _context;
+
+        public CartItemRepository(TAABPDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddCartItemAsync(CartItem cartItem)
+        {
+            await _context.CartItems.AddAsync(cartItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<CartItem> GetCartItemByIdAsync(int cartItemId)
+        {
+            return await _context.CartItems.AsNoTracking().FirstOrDefaultAsync(c=> c.CartItemId == cartItemId);
+        }
+
+        public async Task<List<CartItem>> GetCartItemsByCartIdAsync(int cartId)
+        {
+            return await _context.CartItems.AsNoTracking().Where(c => c.CartId == cartId).ToListAsync();
+        }
+
+        public async Task DeleteCartItemAsync(CartItem cartItem)
+        {
+            _context.CartItems.Remove(cartItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TAABP.Infrastructure/Repositories/ShoppingRepositories/CartRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TAABP.Core.ShoppingEntities;
+
+namespace TAABP.Infrastructure.Repositories.ShoppingRepositories
+{
+    public class CartRepository
+    {
+        private readonly TAABPDbContext _context;
+
+        public CartRepository(TAABPDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddCartAsync(Cart cart)
+        {
+            await _context.Carts.AddAsync(cart);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Cart> GetCartByIdAsync(int cartId)
+        {
+            return await _context.Carts.AsNoTracking().FirstOrDefaultAsync(c => c.CartId == cartId);
+        }
+
+        public async Task GetUserCarts(string userId)
+        {
+            var userCarts = await _context.Carts
+                .Include(c => c.PaymentMethod)
+                .ThenInclude(pm => pm.User)
+                .Where(c => c.PaymentMethod.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task DeleteCartAsync(Cart cart)
+        {
+            _context.Carts.Remove(cart);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
