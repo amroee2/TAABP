d1f771591d7edfa5124156be9a9c19f096611be7
Amro Qadadha <amroqadadha@gmail.com>
2025-01-10 14:27:10 +0200

    Add new PayPalDto with mapping

---
+++ TAABP.Application/Profile/PayPalMapping/PayPalMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using TAABP.Application.DTOs;
+using TAABP.Core.PaymentEntities;
+
+namespace TAABP.Application.Profile.PayPalMapping
+{
+    [Mapper]
+    public partial class PayPalMapper : IPayPalMapper
+    {
+        public partial void PayPalDtoToPayPal(PayPalDto payPalDto, PayPal payPal);
+        public partial PayPalDto PayPalToPayPalDto(PayPal payPal);
+    }
+}
