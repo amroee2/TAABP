6de3e6cbd099ab329524b977e7046fc8bbdfe173
Amro Qadadha <amroqadadha@gmail.com>
2025-01-10 14:01:20 +0200

    Add new PayPal repository class

---
+++ TAABP.Application/RepositoryInterfaces/IPayPalRepository.cs
@@ -0,0 +1,13 @@
+﻿using TAABP.Core.PaymentEntities;
+
+namespace TAABP.Application.RepositoryInterfaces
+{
+    public interface IPayPalRepository
+    {
+        Task<List<PayPal>> GetUserPaymentOptionAsync(string userId);
+        Task<PayPal> GetPaymentOptionByIdAsync(int paymentOptionId);
+        Task AddNewPaymentOptionAsync(PayPal paymentOption);
+        Task UpdatePaymentOptionAsync(PayPal paymentOption);
+        Task DeletePaymentOptionAsync(PayPal paymentOption);
+    }
+}
