c2dc11294996187737b51bb596e9ace93657b4fe
Amro Qadadha <amroqadadha@gmail.com>
2025-01-10 19:20:47 +0200

    Migrate CartItem and Cart Entities to database

--- TAABP.Infrastructure/Migrations/20250110171844_CartAndCartItemMigration.Designer.cs
+++ TAABP.Infrastructure/Migrations/20250110171844_CartAndCartItemMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TAABP.Infrastructure;
 
@@ -11,9 +12,11 @@ using TAABP.Infrastructure;
 namespace TAABP.Infrastructure.Migrations
 {
     [DbContext(typeof(TAABPDbContext))]
-    partial class TAABPDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250110171844_CartAndCartItemMigration")]
+    partial class CartAndCartItemMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
