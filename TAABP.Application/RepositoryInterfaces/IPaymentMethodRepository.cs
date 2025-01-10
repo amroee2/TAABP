545b952dc869bbc4a9478dea3d155146fb21a48b
Amro Qadadha <amroqadadha@gmail.com>
2025-01-10 13:57:30 +0200

    Add new CreditCard and PaymentMethod repositories

---
+++ TAABP.Application/RepositoryInterfaces/IPaymentMethodRepository.cs
@@ -0,0 +1,13 @@
+﻿using TAABP.Core.PaymentEntities;
+
+namespace TAABP.Application.RepositoryInterfaces
+{
+    public interface IPaymentMethodRepository
+    {
+        Task<List<PaymentMethod>> GetUserPaymentMethodsAsync(string userId);
+        Task<PaymentMethod> GetPaymentMethodByIdAsync(int paymentMethodId);
+        Task AddNewPaymentMethodAsync(PaymentMethod paymentMethod);
+        Task UpdatePaymentMethodAsync(PaymentMethod paymentMethod);
+        Task DeletePaymentMethodAsync(PaymentMethod paymentMethod);
+    }
+}
