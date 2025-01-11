b0b5d36e1e98ff79712aa7779ab7053ecdf728d3
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 06:55:49 +0200

    Add new CartItemDto with mappings

---
+++ TAABP.Application/DTOs/ShoppingDto/CartItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TAABP.Application.DTOs.ShoppingDto
+{
+    public class CartItemDto
+    {
+        public int CartItemId { get; set; }
+        public int CartId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
