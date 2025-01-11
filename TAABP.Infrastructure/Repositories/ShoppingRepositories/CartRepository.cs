f9759454af03ae2b9ca0344cb019f1053f38a8c8
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 06:50:13 +0200

    Modify cart repository to include IsCartEmpty and IsRoomAlreadyBooked

--- TAABP.Infrastructure/Repositories/ShoppingRepositories/CartRepository.cs
+++ TAABP.Infrastructure/Repositories/ShoppingRepositories/CartRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TAABP.Application.RepositoryInterfaces;
+using TAABP.Core;
 using TAABP.Core.ShoppingEntities;
 
 namespace TAABP.Infrastructure.Repositories.ShoppingRepositories
 {
-    public class CartRepository
+    public class CartRepository : ICartRepository
     {
         private readonly TAABPDbContext _context;
 
@@ -20,22 +22,44 @@ namespace TAABP.Infrastructure.Repositories.ShoppingRepositories
 
         public async Task<Cart> GetCartByIdAsync(int cartId)
         {
-            return await _context.Carts.AsNoTracking().FirstOrDefaultAsync(c => c.CartId == cartId);
+            var cart = await _context.Carts
+                .AsTracking()
+                .FirstOrDefaultAsync(c => c.CartId == cartId);
+
+            if (cart != null)
+            {
+                _context.Entry(cart).Collection(c => c.CartItems).Query().AsNoTracking();
+            }
+
+            return cart;
         }
 
-        public async Task GetUserCarts(string userId)
+        public async Task<List<Cart>> GetUserCarts(string userId)
         {
             var userCarts = await _context.Carts
                 .Include(c => c.PaymentMethod)
                 .ThenInclude(pm => pm.User)
                 .Where(c => c.PaymentMethod.UserId == userId)
                 .ToListAsync();
+            return userCarts;
         }
 
         public async Task DeleteCartAsync(Cart cart)
         {
             _context.Carts.Remove(cart);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsCartEmpty(int cartId)
+        {
+            return !await _context.CartItems
+                .AnyAsync(ci => ci.CartId == cartId);
+        }
+
+        public async Task<bool> IsRoomAlreadyInCart(int cartId, int roomId)
+        {
+            return await _context.CartItems
+                .AnyAsync(ci => ci.CartId == cartId && ci.RoomId == roomId);
         }
     }
 }
