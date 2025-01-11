bbb5955d5d83b41470696088fcc1f5acf2664ae9
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 15:17:29 +0200

    Add city validation to hotel crud operations

--- TAABP.Application/ServiceInterfaces/IHotelService.cs
+++ TAABP.Application/ServiceInterfaces/IHotelService.cs
@@ -5,9 +5,9 @@ namespace TAABP.Application.ServiceInterfaces
     public interface IHotelService
     {
         Task<int> CreateHotelAsync(int cityId, HotelDto hotelDto);
-        Task<HotelDto> GetHotelByIdAsync(int id);
-        Task<List<HotelDto>> GetHotelsAsync();
-        Task DeleteHotelAsync(int id);
+        Task<HotelDto> GetHotelByIdAsync(int cityId, int id);
+        Task<List<HotelDto>> GetHotelsAsync(int hotelId);
+        Task DeleteHotelAsync(int cityId, int id);
         Task UpdateHotelAsync(int Id, HotelDto hotelDto);
         Task<int> CreateNewHotelImageAsync(int Id, HotelImageDto hotelImageDto);
         Task<HotelImageDto> GetHotelImageByIdAsync(int hotelId, int imageId);
