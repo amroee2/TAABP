58dc8e1fc791e457b7bfdbe020d35b5703a60c07
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 16:12:30 +0200

    Rename GetUserCarts method

--- TAABP.Application/RepositoryInterfaces/ICartRepository.cs
+++ TAABP.Application/RepositoryInterfaces/ICartRepository.cs
@@ -7,7 +7,7 @@ namespace TAABP.Application.RepositoryInterfaces
         Task<Cart> GetCartByIdAsync(int cartId);
         Task AddCartAsync(Cart cart);
         Task DeleteCartAsync(Cart cart);
-        Task<List<Cart>> GetUserCarts(string userId);
+        Task<List<Cart>> GetUserCartsAsync(string userId);
         Task<bool> IsCartEmpty(int cartId);
         Task<bool> IsRoomAlreadyInCart(int cartId, int roomId);
     }
