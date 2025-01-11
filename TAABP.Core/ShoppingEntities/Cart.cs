d466ec69a08f7bd6688dd8b2016788ac29bb9f47
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 06:54:46 +0200

    Modify Cart class and migrate to database
    Added new CartStatus column and made PaymentMethod nullable, and add AddToPrice and RemoveFromPrice methods

--- TAABP.Core/ShoppingEntities/Cart.cs
+++ TAABP.Core/ShoppingEntities/Cart.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
 using TAABP.Core.PaymentEntities;
 
 namespace TAABP.Core.ShoppingEntities
@@ -9,11 +10,23 @@ namespace TAABP.Core.ShoppingEntities
         public int CartId { get; set; }
         public List<CartItem> CartItems { get; set; }
         public double TotalPrice { get; set; }
-        public int PaymentMethodId { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
+        public int? PaymentMethodId { get; set; }
+        public PaymentMethod? PaymentMethod { get; set; }
+        public CartStatus CartStatus { get; set; }
         public Cart()
         {
+
             CartItems = new List<CartItem>();
+        }
+
+        public void AddToPrice(double price)
+        {
+            TotalPrice += price;
+        }
+
+        public void RemoveFromPrice(double price)
+        {
+            TotalPrice -= price;
         }
     }
 }
