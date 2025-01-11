558269dc2122a98b54ca7aebf684f10d1ebcc433
Amro Qadadha <amroqadadha@gmail.com>
2025-01-11 06:57:36 +0200

    Add new RoomAlreadyBooked Exception

---
+++ TAABP.Application/Exceptions/RoomAlreadyBookedException.cs
@@ -0,0 +1,17 @@
+﻿namespace TAABP.Application.Exceptions
+{
+    public class RoomAlreadyBookedException : Exception
+    {
+        public RoomAlreadyBookedException(string message) : base(message)
+        {
+        }
+
+        public RoomAlreadyBookedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public RoomAlreadyBookedException()
+        {
+        }
+    }
+}
